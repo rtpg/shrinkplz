8bd96bdd193123b4a3cd7010ee2a102f08117e46
Raphael Gaschignard <raphael@rtpg.co>
2025-08-10 18:16:40 +1000

    rework example script to take failure cases from a file
    This is preliminary work for testing

--- example/my-prog.py
+++ example/my-prog.py
@@ -1,17 +1,22 @@
 """
-My program, that fails when I have D + U + V + W in the set
+Pass in a list of failure cases in the first argument
+
+Read in lines.
+
+Return failure if all the failure cases were present
+in the input
 """
 
 import sys
 
-failures = ["D", "U", "V", "W"]
+with open(sys.argv[1], "r") as f:
+    failures = [line.strip() for line in f.readlines()]
 in_lines = list(sys.stdin)
 
 has_failed_tests = all(f"{failed_case}\n" in in_lines for failed_case in failures)
 
 if has_failed_tests:
     print("FAILED")
-    sys.exit(1)
 else:
     print("SUCCEEDED")
 sys.exit(1 if has_failed_tests else 0)
